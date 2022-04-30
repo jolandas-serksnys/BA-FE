a65fe967e4fc8113be4b22e2e993c392d5f7c0b4
Jolandas Šerkšnys <jolandas.serksnys@ktu.edu>
2022-05-01 01:02:21 +0300

    fixed front testing

--- src/components/pages/admin/Admin.test.tsx
+++ src/components/pages/admin/Admin.test.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
 import { AdminPage } from './Admin';
+import { withQueryClient } from '@src/utils/__mocks__/withQueryClient';
+import { withMemoryRouter } from '@src/utils/__mocks__/withMemoryRouter';
+import api from '@src/utils/api';
+
+jest.mock('@src/utils/api');
 
 describe('Admin', () => {
+  (api.get as jest.Mock).mockImplementation(() => undefined);
   it('should render correctly', () => {
-    render(<AdminPage />);
+    render(withQueryClient(withMemoryRouter(<AdminPage />)));
 
     expect(screen.getByText(/establishment/i)).toBeInTheDocument();
   });
