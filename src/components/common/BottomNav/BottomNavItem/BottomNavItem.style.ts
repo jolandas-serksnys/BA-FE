2e7af7d0e1621651cfff2bb8ded81916d32d6e4c
Jolandas Šerkšnys <jolandas.serksnys@ktu.edu>
2022-05-26 17:46:37 +0300

    Style fixes

--- src/components/common/BottomNav/BottomNavItem/BottomNavItem.style.ts
+++ src/components/common/BottomNav/BottomNavItem/BottomNavItem.style.ts
@@ -6,4 +6,15 @@ export const Li = styled.li`
   a {
     height: 100%;
   }
+
+  @media (max-width: 768px) {
+    a {
+      display: flex;
+      flex-direction: column;
+      font-size: 0.8rem;
+      text-align: center;
+      padding: 0.5rem !important;
+      gap: 0.25rem !important;
+    }
+  }
 `;
